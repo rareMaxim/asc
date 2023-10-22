30384f43b08bd3da8b64605c36f94e93545be7a8
Maxim S <maks4a@gmail.com>
2023-10-22 17:49:35 +0300

    wip - diia integration

---
+++ asc/asc_core/doctype/asc_diia_service_status/asc_diia_service_status.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Максим Сисоєв and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("ASC Diia Service Status", {
+// 	refresh(frm) {
+
+// 	},
+// });
