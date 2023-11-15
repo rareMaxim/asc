a288af9347140190e43a98a67189b13ef3c2377d
Maxim S <maks4a@gmail.com>
2023-11-15 08:26:46 +0200

    Shedulte test

---
+++ asc/asc_core/doctype/asc_shedule_item/asc_shedule_item.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2023, Максим Сисоєв and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ASCSheduleItem(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		is_work: DF.Check
+		title: DF.Data | None
+	# end: auto-generated types
+	pass
